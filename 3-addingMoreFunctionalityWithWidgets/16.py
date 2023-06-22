bac635bc9585a0b3acb277b708de2a3f4f3a6055
jonasht <jhenriquet@outlook.com.br>
2023-06-21 23:45:42 -0300

    new studies about book

--- 3-addingMoreFunctionalityWithWidgets/16.py
+++ 3-addingMoreFunctionalityWithWidgets/16.py
@@ -44,7 +44,65 @@ class LoginWindow(QWidget):
         self.le_password.resize(250, 24)
         self.le_password.move(90, 82)
         
-        # continuar ...
+        self.cb_showPassword = QCheckBox('show password', self)
+        self.cb_showPassword.move(90, 110)
+
+        self.cb_showPassword.toggled.connect(self.displayPasswordIfChecked)
+
+        # 
+        bt_login = QPushButton('login', self)
+        bt_login.resize(320, 34)
+        bt_login.move(20, 140)
+        bt_login.clicked.connect(self.clickLoginButton)
+
+        lb_not_member = QLabel('not a member?', self)
+        lb_not_member.move(20, 186)
+
+        bt_sign_up = QPushButton('sign up', self)
+        bt_sign_up.move(120, 180)
+        bt_sign_up.clicked.connect(self.createNewUser)
+    
+    def clickLoginButton(self):
+        users = {}
+        file = 'file/users.txt'
+
+        try:
+            with open(file, 'r') as reader:
+                for line in reader:
+                    user_info = line.split(' ')
+                    username_info = user_info[0]
+                    password_info = user_info[1].strip('\n')
+                    users[username_info] = password_info
+            
+            username = self.le_username.text()
+            password = self.le_password.text()
+
+            if (username, password) in users.items():
+                QMessageBox.information(self,
+                                        'login successful',
+                                        'login sucessful',
+                                        QMessageBox.StandardButton.Ok,
+                                        QMessageBox.StandardButton.Ok
+                                        )
+                self.login_is_successful =True
+                self.close()
+                self.openApplicationWindow()
+            else:
+                QMessageBox.warning(self, 'error message', 'the username or password is incorrect', 
+                                    QMessageBox.StandardButton.Close,
+                                    QMessageBox.StandardButton.Close)
+
+        except FileNotFoundError as error:
+            QMessageBox.warning(self, 'error',
+                                f'''<p> file not found </p>''',
+                                f'''error: {error} ''',
+                                QMessageBox.StandardButton.Ok
+                                
+                                )
+            
+            # create file if it doesnt exit/ criar file se ele nao existe
+            f = open(file, 'w')
+            
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
